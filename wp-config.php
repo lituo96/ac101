006acd91cb9671c56ffa6b4c1208f748c5401d9a
lituo <971697820@qq.com>
2018-05-17 16:09:37 +0800

    2
    2

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,18 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', $_ENV['MYSQL_DBNAME']);
+define('DB_NAME', $_ENV['MYSQL_DATABASE']);
 
 /** MySQL数据库用户名 */
-define('DB_USER', 	$_ENV['MYSQL_USERNAME']);
+define('DB_USER', 	$_ENV['MYSQL_USER']);
 
 /** MySQL数据库密码 */
 define('DB_PASSWORD', $_ENV['MYSQL_PASSWORD']);
 
 /** MySQL主机 */
-define('DB_HOST',$_ENV['MYSQL_HOST']);
+define('DB_HOST',$_ENV['MYSQL_SERVICE_HOST
+
+']);
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8mb4');
